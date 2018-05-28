4807d348277f6af668a55bec7c73247eb6b11614
shiveen.pandita <shiveenpandita@outlook.com>
2018-05-28 21:43:25 +1000

    Initial commit

---
+++ torrent-client/src/main/kotlin/com/shiveenp/Application.kt
@@ -0,0 +1,11 @@
+package com.shiveenp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
